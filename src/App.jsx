a13f2fb5dabcd82af2ae0883b261ac680f288dcf
Ariel Martin <arielmartindev@gmail.com>
2025-05-20 11:17:55 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <>
+    
+    <Container>
+      <h1>Proyecto React 2025</h1>
+    </Container>
+    
+    </>
+  )
+}
+
+export default App
