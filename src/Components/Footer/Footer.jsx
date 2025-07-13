fb4d035fb60266fa350683e1d6b0d140d53ebfef
Ariel Martin <107778014+arielmartindev@users.noreply.github.com>
2025-07-13 01:05:29 -0300

    footer text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css";
 
 export const Footer = () => {
-  return <div className="footer-container justify-content-center">Footer</div>;
+  return <div className="footer-container d-flex justify-content-center align-items-center">© All Rights Reserved By Ariel Martin Web Developer<br/>Proyecto Final React 25017</div>;
 };
 
 export default Footer;
